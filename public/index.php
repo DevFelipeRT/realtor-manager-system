325e72dc6054510e32567e8fee8ef743d0d253bb
Felipe Ruiz Terrazas <feliperterrazas@gmail.com>
2025-04-11 12:46:14 -0300

    url fix

--- public/index.php
+++ public/index.php
@@ -4,8 +4,6 @@ use App\Controllers\RealtorController;
 
 require_once __DIR__ . '/../config/config.php';
 
-var_dump(PUBLIC_URL);
-
 $realtorController = new RealtorController();
 $realtorController->postRequestProcess();
 $realtorController->index();
